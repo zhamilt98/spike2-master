8fb6a78a80242dc22caddaf5c794b6dd9d4305d8
kwainright <kwainrig@uncc.edu>
2021-10-17 18:14:51 -0400

    wip

---
+++ controller/router.js
@@ -0,0 +1,52 @@
+'use strict';
+const router = require('koa-router')();
+const send = require('koa-send');
+
+const db = require('../db');
+const myDb = new db();
+
+// router.get('/login', async (ctx, next) => {
+// 	try {
+// 		await send(ctx, './react-app/build/login.html');
+// 	} catch(err) {
+// 		console.log('error in / route');
+// 		return next();
+// 	}
+// });
+// router.get('/signup', async (ctx, next) => {
+// 	try {
+// 		await send(ctx, './react-app/build/signup.html');
+// 	} catch(err) {
+// 		console.log('error in / route');
+// 		return next();
+// 	}
+// });
+
+router.get('/fetchAll', async (ctx, next) => {
+	try {
+		const results = await myDb.queryAll();
+		console.log(results);
+		ctx.body = results;
+	} catch(err) {
+		// TODO: handle err?
+		return next();
+	}
+});
+
+router.get('/search', async (ctx, next) => {
+	const search_route = require('../routes/search-route');
+
+	ctx.body = await search_route(ctx);
+	console.log('made post');
+});
+
+router.get('/(.*)', async (ctx, next) => {
+	try {
+		await send(ctx, './react-app/build/index.html');
+	} catch(err) {
+		console.log('error in / route');
+		return next();
+	}
+});
+
+module.exports = router;
